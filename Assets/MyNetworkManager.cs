1b4b377a325864bc719535488c4fe3d12e0f811d
mikeohl <mikeohl@live.com>
2018-02-08 23:02:02 -0800

    Scripts for Ahoy Matey RTS Multiplayer Network Game

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MyStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " : Starting Host at ");
+        StartHost();
+    }
+
+    public override void OnStartHost ()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " : Host Started at ");
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " : Client Start Requested");
+        InvokeRepeating("PrintDots", 0f, 1f);
+        //base.OnStartClient(client);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        CancelInvoke();
+        Debug.Log(Time.timeSinceLevelLoad + " : Client is connected to IP: " + conn.address);
+        //base.OnClientConnect(conn);
+    }
+
+    void PrintDots ()
+    {
+        Debug.Log(".");
+    }
+}
---
+++ Assets/Players/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 input;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLocalPlayer)
+        {
+            input.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            input.z = CrossPlatformInputManager.GetAxis("Vertical");
+            input.y = 0f;
+
+            transform.Translate(input);
+        }
+
+	}
+}
